06815ef340510fe46618bb44a0f02911a4257638
Brent Waters <bcwaters@asu.edu>
2020-06-30 07:20:05 -0700

    setup express server for ec2 instance

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const fs = require('fs');
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/text',(req, res) => {
   
+    fs.appendFile('text.log','</br>---- start msg ---- </br></br>' + req.body.phone_number + ': ' + req.body.text + '</br>raw data:   ' +  JSON.stringify(req.body) + '</br></br>------- end msg -----</br>', function (err) {
+        if (err) throw err;
+        
+        
+    });
+    
   console.log("text recieved:\n" + JSON.stringify(req.body));
-  res.end("yes");
+  res.end(JSON.stringify(req.body));
 });
 
-
+app.use(function(req, res){
+    fs.readFile("text.log", "utf8", function(err, data) {  res.send(data); if(err) res.send('error')});
+     
+ });
 
 app.listen(3000,() => {
   console.log("Started on PORT 3000");
